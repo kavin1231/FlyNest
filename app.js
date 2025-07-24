aa072e227427fc768261b34c0b0074c331717442
kavin1231 <chamathkaveendra@gmail.com>
2025-07-24 13:08:34 +0530

    done

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+let mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
+
 app.listen(3005, () => {
   console.log("Server is running on port 3005");
 });
